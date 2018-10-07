34a466bb52f9da7c101d437434b8d9d5707cee05
SDX <realsdx0@gmail.com>
2018-10-08 00:12:02 +0530

    Added Level viewset

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from backend import views
+from rest_framework import routers
 
-urlpatterns = [
-    path('',views.index, name="index"),
+router = routers.DefaultRouter()
+
+router.register(r'users', views.UserViewSet)
+router.register(r'level', views.LevelViewSet)
+
+urlpatterns = router.urls;
+
+urlpatterns += [
     path('leaderboard/', views.leaderboard, name="leaderborad"),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,17 +1,40 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-from backend.models import Player
-from django.core import serializers
+from backend.models import Player, Level, Location
+from django.contrib.auth.models import User
+from django.core import serializers as ds
+from rest_framework import serializers, viewsets
 import json
 
-# Create your views here.
+#DRF viewset and serializers
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = ('level_no','title','ques')
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+
 def index(req):
     data = {"page":"backend"}
     return JsonResponse(json.dumps(data), safe=False)
 
 def leaderboard(req):
     data = Player.objects.all().order_by('-score')
-    data = json.loads(serializers.serialize("json", data))
+    data = json.loads(ds.serialize("json", data))
     api_data = []
     for i in data:
         api_data.append(i.get('fields'))
